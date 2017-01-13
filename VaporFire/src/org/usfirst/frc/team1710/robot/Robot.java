083bb426a9fc1254febf5aaccb4ab9c727b405f0
IsaacBeech <imbeech37@gmail.com>
2017-01-13 14:58:04 -0600

    Initial stuff created

--- VaporFire/src/org/usfirst/frc/team1710/robot/Robot.java
+++ VaporFire/src/org/usfirst/frc/team1710/robot/Robot.java
@@ -1,7 +1,12 @@
 
 package org.usfirst.frc.team1710.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -18,7 +23,10 @@ public class Robot extends IterativeRobot {
     final String customAuto = "My Auto";
     String autoSelected;
     SendableChooser chooser;
-	
+	double turn, forward, multiplier, climbPower;
+	int axisType, gear;
+	boolean turboActivate;
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -28,6 +36,17 @@ public class Robot extends IterativeRobot {
         chooser.addDefault("Default Auto", defaultAuto);
         chooser.addObject("My Auto", customAuto);
         SmartDashboard.putData("Auto choices", chooser);
+        RobotMap.driveStick = new Joystick(0);
+        RobotMap.mechStick = new Joystick(1);
+        RobotMap.move = new RobotDrive(0,1,2,3);
+        RobotMap.climber = new Talon(4);
+        RobotMap.shooter = new Talon(5);
+        CameraServer camera = CameraServer.getInstance();
+        camera.setQuality(50);
+        camera.startAutomaticCapture("Front Camera");
+        turboActivate = false;
+        axisType = 1;
+        RobotMap.shifter = new DoubleSolenoid(1,2);
     }
     
 	/**
@@ -64,7 +83,30 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+    	turn = RobotMap.driveStick.getRawAxis(axisType);
+    	forward = RobotMap.driveStick.getRawAxis(0);
+    	multiplier = RobotMap.driveStick.getRawAxis(3)*.5+.5;
+    	turboActivate = RobotMap.driveStick.getRawButton(1);
+    	if(turboActivate == true){
+    		//engage shift
+    		RobotMap.shifter.set(1,2);
+        	RobotMap.move.arcadeDrive(forward*multiplier, turn*.3);
+    		axisType = 2;
+    		gear = 2;
+    	}
+    	else{
+    		//disengage shift
+        	RobotMap.move.arcadeDrive(forward*multiplier, turn*multiplier);
+    		axisType = 1;
+    		gear = 1;
+    		
+    	}
+    	//***************************
+    	climbPower = RobotMap.mechStick.getRawAxis(1);
+        SmartDashboard.putNumber("Gear", gear);
+        SmartDashboard.putNumber("Multiplier", multiplier);
         
+    	
     }
     
     /**
