4cd6abeb085f681b08d21d9c10d0edd2e11343c1
Jake Herman <thisguyherm@gmail.com>
2017-02-06 16:07:01 -0600

    added new shooter stuff

--- VaporFire/src/org/usfirst/frc/team1710/robot/Shooter.java
+++ VaporFire/src/org/usfirst/frc/team1710/robot/Shooter.java
@@ -21,22 +21,22 @@ public class Shooter extends Subsystem {
        power = 0;
     }
     public static void run(){
-    	currentVelocity = RobotMap.Shooter1.getEncVelocity();
+    	currentVelocity = RobotMap.shooter.getEncVelocity();
     	error = ((goalVelocity - currentVelocity) / (goalVelocity + currentVelocity));
     	power -= (inc * error);
     	Timer.delay(.25);
-    	RobotMap.Shooter1.set(power);
-    	RobotMap.Shooter2.set(power);
+    	RobotMap.shooter.set(power);
     	SmartDashboard.putNumber("Motor Power", power);
     	SmartDashboard.putNumber("Velocity", currentVelocity);
+    
     }
+    
     public static void testMode() {
-    	RobotMap.Shooter1.set(-0.3);
-    	RobotMap.Shooter2.set(-0.3);
+    	RobotMap.shooter.set(-0.3);
     }
+    
     public static void stopShooter(){ 
-    	RobotMap.Shooter1.set(0);
-    	RobotMap.Shooter2.set(0);
+    	RobotMap.shooter.set(0);
     }
 }
 
