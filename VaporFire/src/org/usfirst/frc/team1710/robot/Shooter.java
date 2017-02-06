7029c6ee1870f5a5f63a00ba19b3e6c1e541ded8
Jake Herman <thisguyherm@gmail.com>
2017-02-06 16:17:47 -0600

    i cheated

--- VaporFire/src/org/usfirst/frc/team1710/robot/Shooter.java
+++ VaporFire/src/org/usfirst/frc/team1710/robot/Shooter.java
@@ -12,23 +12,31 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Shooter extends Subsystem {
 
 	static double currentVelocity;
-	static double inc = 0.08;
+	static double inc = 0.1;
 	static double power = 0;
 	static double error = 0;
 	static int goalVelocity = -28000;
 	
+	
+	
     public void initDefaultCommand() {
        power = 0;
     }
-    public static void run(){
+    public static void runShooter(){
     	currentVelocity = RobotMap.shooter.getEncVelocity();
     	error = ((goalVelocity - currentVelocity) / (goalVelocity + currentVelocity));
     	power -= (inc * error);
-    	Timer.delay(.25);
+    	Timer.delay(.1);
     	RobotMap.shooter.set(power);
     	SmartDashboard.putNumber("Motor Power", power);
     	SmartDashboard.putNumber("Velocity", currentVelocity);
     
+    }
+    public static void runIndexer(){
+    	RobotMap.indexer.set(1);
+    }
+    public static void stopIndexer(){
+    	RobotMap.indexer.set(0);
     }
     
     public static void testMode() {
@@ -38,5 +46,14 @@ public class Shooter extends Subsystem {
     public static void stopShooter(){ 
     	RobotMap.shooter.set(0);
     }
+    public static void runSystem(){
+    	currentVelocity = RobotMap.shooter.getEncVelocity();
+    	if(currentVelocity > goalVelocity - 500 && currentVelocity < goalVelocity + 500){
+    		runShooter();
+    		runIndexer();
+    	}else{
+    		stopIndexer();
+    		runShooter();
+    	}
+    }
 }
-
