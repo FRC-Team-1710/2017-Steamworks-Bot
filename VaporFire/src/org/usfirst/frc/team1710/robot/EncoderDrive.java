8b2cbd6957ebe14eb1a7c5dae8b988fb56640c04
spencersliffe <spencer.s1@ravenengineer.org>
2017-02-11 18:00:26 -0600

    mehotion profiling

--- VaporFire/src/org/usfirst/frc/team1710/robot/EncoderDrive.java
+++ VaporFire/src/org/usfirst/frc/team1710/robot/EncoderDrive.java
@@ -12,6 +12,8 @@ public class EncoderDrive extends Command {
 	
 	double rotations, rotateToPublic;
 	
+	double[] motionProfile = {90,90,90,90,90,90,90,90,90,90,90,60,60,60,60,30,30,30,30,30,30,30,30};
+	int count;
 	long startTime, endTime, timeElapsed, currentEncoder, startEncoder, rotationVal;
     public EncoderDrive(double rotateTo, double goalVelocity) {
         // Use requires() here to declare subsystem dependencies
@@ -28,28 +30,34 @@ public class EncoderDrive extends Command {
     	loRotationAdded = false;
     	startTime = System.nanoTime() / 1000000000;
     	inc = 0.3;
+    	count = 0;
+    	power = 0.8;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	angle = (RobotMap.REncoder.getVoltage() * 360/5);
-    	System.out.println(rotations);
     	if(rotations < rotateToPublic){
-    		Drive.simpleArcade(-power, 0, 1);
+    		Drive.simpleArcade(power, 0, 1);
     		if(angle > 179 && hiRotationAdded == false) {
         		rotations += 0.5;
         		hiRotationAdded = true;
         		loRotationAdded = false;
-        		endTime = System.nanoTime();
+        		endTime = System.nanoTime()/1000000000;
         	} else if(angle <= 179 && angle >= 0 && loRotationAdded == false) {
         		rotations += 0.5;
         		loRotationAdded = true;
         		hiRotationAdded = false;
-        		endTime = System.nanoTime();
+        		endTime = System.nanoTime()/1000000000;
         	}
     		timeElapsed = endTime - startTime;
         	distance = rotations * Math.PI * 4;
     		currentVelocity = distance / timeElapsed;
+    		System.out.println(currentVelocity);
+    		goalVelocityPublic = motionProfile[count];
+    		if(count < motionProfile.length - 1) {
+    			count ++;
+    		}
         	error = ((goalVelocityPublic - currentVelocity) / (goalVelocityPublic + currentVelocity));
         	power -= (inc * error);
     	}else{
@@ -73,7 +81,6 @@ public class EncoderDrive extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	System.out.println(RobotMap.REncoder.getAccumulatorCount());
     }
 
     // Called when another command which requires one or more of the same
--- VaporFire/src/org/usfirst/frc/team1710/robot/EncoderTest.java
+++ VaporFire/src/org/usfirst/frc/team1710/robot/EncoderTest.java
@@ -10,6 +10,8 @@ public class EncoderTest extends CommandGroup {
     public EncoderTest() {
     	addSequential(new ZeroYaw());
     	//low gear is 4 to 1 so if it was 1 to 1, 9.5 would move us ten feet but 9.5 * 4 = 38 so 38 "rotations" get us 10 ft
-    	addSequential(new EncoderDrive(38, .5));
+    	//@param rotations
+    	//@param velocity
+    	addSequential(new EncoderDrive(38, 100));
     }
 }
