ee5d9913db65df1c97574144007ad045154aad89
Jake Herman <thisguyherm@gmail.com>
2017-04-20 11:11:35 -0500

    added more waypoints
    for hopper shoot curve

--- VaporFire/src/trajectoryGeneration/Waypoints.java
+++ VaporFire/src/trajectoryGeneration/Waypoints.java
@@ -17,13 +17,21 @@ public class Waypoints {
     
     //TODO: adjust distances
     //this path snakes right up to the button and does a swift, clean turn back and forth to release the balls
-    public static Waypoint[] hopperShootBlue = new Waypoint[] {
+    public static Waypoint[] hopperShootBlueSnake = new Waypoint[] {
         new Waypoint(1, 0, 0),
         new Waypoint(2, 1, 0),
         new Waypoint(3, 2, 0),
         new Waypoint(4, 2, 0),
         new Waypoint(5, 2, 0),
         new Waypoint(6, 2, 45),
+    };
+    
+    //TODO:adjust distances
+    //this path starts parallel to the boiler then curves right into the hopper button
+    public static Waypoint[] hopperShootRedCurve = new Waypoint[] {
+        new Waypoint(0, 0, 0),
+        new Waypoint(.5, 0, 0),
+        new Waypoint(1.3, .6, -90),
     };
     
     public static Waypoint[] testPoints = new Waypoint[] {
