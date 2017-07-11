4d2d4e72e5bf2077517f6cec840971f450f5db59
Jake Herman <team1710.cto@gmail.com>
2017-07-11 11:52:00 -0500

    use drive to position

--- VaporFire/src/org/usfirst/frc/team1710/robot/commandGroups/GearPlaceLeft.java
+++ VaporFire/src/org/usfirst/frc/team1710/robot/commandGroups/GearPlaceLeft.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1710.robot.commandGroups;
 
+import org.usfirst.frc.team1710.robot.Commands.DriveToPosition;
 import org.usfirst.frc.team1710.robot.Commands.EncoderDrive;
 import org.usfirst.frc.team1710.robot.Commands.RotatetoAngle;
 import org.usfirst.frc.team1710.robot.Commands.ZeroYaw;
@@ -13,7 +14,7 @@ public class GearPlaceLeft extends CommandGroup {
 	//for red alliance, would be right for the blue alliance
     public GearPlaceLeft() {
     	addSequential(new ZeroYaw());
-    	addSequential(new EncoderDrive(90, .8, false));
+    	addSequential(new DriveToPosition(90, 0, .8));
     	addSequential(new ZeroYaw());
     	addSequential(new RotatetoAngle(70));
     	addSequential(new ZeroYaw());
--- VaporFire/src/org/usfirst/frc/team1710/robot/commandGroups/GearPlaceRight.java
+++ VaporFire/src/org/usfirst/frc/team1710/robot/commandGroups/GearPlaceRight.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1710.robot.commandGroups;
 
+import org.usfirst.frc.team1710.robot.Commands.DriveToPosition;
 import org.usfirst.frc.team1710.robot.Commands.EncoderDrive;
 import org.usfirst.frc.team1710.robot.Commands.RotatetoAngle;
 import org.usfirst.frc.team1710.robot.Commands.ZeroYaw;
@@ -13,7 +14,9 @@ public class GearPlaceRight extends CommandGroup {
 	//for red alliance, would be left for the blue alliance
     public GearPlaceRight() {
     	addSequential(new ZeroYaw());
-    	addSequential(new EncoderDrive(90, .8, false));
+    	//drives 90 inches forward holding a heading of 0 degrees (straight)
+    	//at 80% speed, do change the direction just make the distance negative.
+    	addSequential(new DriveToPosition(90, 0, .8));
     	addSequential(new ZeroYaw());
     	addSequential(new RotatetoAngle(-70));
     	addSequential(new ZeroYaw());
