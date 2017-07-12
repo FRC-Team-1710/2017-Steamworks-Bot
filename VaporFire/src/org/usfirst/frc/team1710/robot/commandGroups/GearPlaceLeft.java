27c0557d1c0fb8d185368f9d66c76b5734c8effe
Jake Herman <thisguyherm@gmail.com>
2017-07-12 02:48:00 -0500

    perfecttt

--- VaporFire/src/org/usfirst/frc/team1710/robot/commandGroups/GearPlaceLeft.java
+++ VaporFire/src/org/usfirst/frc/team1710/robot/commandGroups/GearPlaceLeft.java
@@ -14,10 +14,12 @@ public class GearPlaceLeft extends CommandGroup {
 	//for red alliance, would be right for the blue alliance
     public GearPlaceLeft() {
     	addSequential(new ZeroYaw());
-    	addSequential(new DriveToPosition(90, 0, .8));
+    	//drives 90 inches forward holding a heading of 0 degrees (straight)
+    	//at 80% speed, do change the direction just make the distance negative.
+    	addSequential(new DriveToPosition(74, 0, .65));
     	addSequential(new ZeroYaw());
-    	addSequential(new RotatetoAngle(70));
+    	addSequential(new RotatetoAngle(50));
     	addSequential(new ZeroYaw());
-    	addSequential(new DriveToPosition(36, 0, .4));
+    	addSequential(new DriveToPosition(27, 0, .4));
     }
 }
